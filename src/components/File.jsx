a01eff9a86ef4d11183c79d342cffbd91f6b53a1
Yuvam Kumar <yuvamk.swe@gmail.com>
2024-11-10 21:06:19 -0800

    Updated File.jsx

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -2,6 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenFancy } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from "prop-types";
+import GradualSpacing from './ui/gradual-spacing';
 
 export default function File(props) {
 
@@ -20,11 +21,13 @@ export default function File(props) {
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 justify-center items-center w-fit max-w-full mx-auto' >
 
-        <h1 className="text-white text-4xl font-medium sm:text-6xl " >Your File</h1>
+        <h1 className="text-white text-4xl font-medium sm:text-6xl " >
+            <GradualSpacing text={'Your File'} />
+        </h1>
 
         <div className=' flex flex-col text-left my-4 text-white items-center mx-auto'>
                 <h3 className='font-bold'>Name</h3>
-                <p className='truncate text-red-600'>{file ? file.name : 'Custom audio'}</p>
+                <p className='truncate text-black'>{file ? file.name : 'Custom audio'}</p>
         </div>
 
         <div className='flex items-center justify-between gap-4'>
